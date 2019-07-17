3d4531fd48e6d21d2b122e4e92d7b2c009e1f060
Chris Pioli <christopher.pioli@gmail.com>
2019-07-17 14:25:01 -0700

    Added erasure methods to LinkedList

--- PracticeProblems/LinkedList.h
+++ PracticeProblems/LinkedList.h
@@ -34,7 +34,11 @@ public:
 	void push_front(const T &_value);
 	void pop_front();
 	void push_back(const T &_value);
-	bool empty() { return head == nullptr; }
+	bool insert_after(const T &_beforeValue, const T &_insertValue);
+	bool insert_before(const T &_nextValue, const T &_insertValue);
+	bool erase(const T &_deleteValue);
+	bool erase_after(const T &_beforeValue);
+	bool empty() { return m_head == nullptr; }
 	size_t length();
 	friend std::ostream & operator<<(std::ostream &out, const LinkedList<T>& list)
 	{
@@ -56,12 +60,7 @@ LinkedList<T>::LinkedList(const T &_value) {
 
 template<typename T>
 void LinkedList<T>::clear() {
-	while (m_length > 0) {
-		m_cursor = m_head;
-		m_head = m_head->next;
-		delete m_cursor;
-		m_length--;
-	}
+	delete m_head; //should propogate with the destructor
 }
 
 template<typename T>
@@ -77,7 +76,7 @@ void LinkedList<T>::push_front(const T &_value) {
 
 template<typename T>
 void LinkedList<T>::pop_front() {
-	if (m_length == nullptr) return;
+	if (empty()) return;
 	m_cursor = m_head;
 	m_head = m_head->next;
 	delete m_cursor;
@@ -88,13 +87,93 @@ void LinkedList<T>::pop_front() {
 template<typename T>
 void LinkedList<T>::push_back(const T &_value) {
 	LinkedListNode<T> *newNode = new LinkedListNode<T>(_value, nullptr);
-	if (m_head == nullptr) {
+	if (empty()) {
 		m_head = newNode;
 	} else {
 		m_tail->next = newNode;
 	}
 	m_tail = newNode;
 	m_length++;
+}
+
+template<typename T>
+bool LinkedList<T>::insert_after(const T &_beforeValue, const T &_insertValue) {
+	if (empty()) return false;
+	m_cursor = m_head;
+	while (m_cursor != nullptr) {
+		if (m_cursor->m_value == _beforeValue) {
+			LinkedListNode<T> *newNode = new LinkedListNode<T>(_insertValue, m_cursor->next);
+			m_cursor->next = newNode;
+			m_length++;
+			return true;
+		}
+		m_cursor = m_cursor->next;
+	}
+	return false;
+}
+
+template<typename T>
+bool LinkedList<T>::insert_before(const T &_nextValue, const T &_insertValue) {
+	if (empty()) return false;
+	m_cursor = m_head;
+	if (m_cursor->m_value == _nextValue) { //if _insertValue needs to go into head
+		push_front(_insertValue);
+		return true;
+	}
+	LinkedListNode<T> *pre_cursor = m_cursor;
+	m_cursor = m_cursor->next;
+	while (m_cursor != nullptr) {
+		if (m_cursor->m_value == _nextValue) {
+			LinkedListNode<T> *newNode = new LinkedListNode<T>(_insertValue, m_cursor);
+			pre_cursor->next = newNode;
+			m_length++;
+			return true;
+		}
+		m_cursor = m_cursor->next;
+		pre_cursor = pre_cursor->next;
+	}
+	return false;
+}
+
+template<typename T>
+bool LinkedList<T>::erase(const T &_deleteValue) {
+	if (empty()) return false;
+	if (m_head->m_value == _deleteValue) {
+		pop_front();
+		return true;
+	}
+	m_cursor = m_head->next;
+	LinkedListNode<T> *pre_cursor = m_head;
+	while (m_cursor->next != nullptr) {
+		if (m_cursor->m_value == _deleteValue) {
+			pre_cursor->next = m_cursor->next;
+			m_cursor->next = nullptr; //without this the destructor will propogate through the rest of the linked list
+			delete m_cursor;
+			m_length--;
+			return true;
+		}
+		m_cursor = m_cursor->next;
+	}
+	return false;
+}
+
+template<typename T>
+bool LinkedList<T>::erase_after(const T &_beforeValue) {
+	if (empty()) return false;
+	m_cursor = m_head;
+	while (m_cursor != nullptr) {
+		if (m_cursor->m_value == _beforeValue) {
+			LinkedListNode<T> *temp = m_cursor->next;
+			m_cursor->next = temp->next;
+			if (m_tail == temp) m_tail = m_cursor;
+			else temp->next = nullptr;
+			delete temp;
+			m_length--;
+			return true;
+		}
+		m_cursor = m_cursor->next;
+	}
+	return false;
 }
 
 template<typename T>
