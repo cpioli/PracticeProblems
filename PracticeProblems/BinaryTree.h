42492d9760ca67bfd602004823a4e49cb3568e5b
Chris Pioli <christopher.pioli@gmail.com>
2019-07-31 13:17:00 -0700

    BST Erase method implemented

--- PracticeProblems/BinaryTree.h
+++ PracticeProblems/BinaryTree.h
@@ -19,6 +19,8 @@ private:
 	BinaryTreeNode<T> *m_root, *m_cursor;
 
 	void insert(const T& _value, BinaryTreeNode<T> *root);
+	BinaryTreeNode<T>* erase(BinaryTreeNode<T> *root, const T& _value);
+	BinaryTreeNode<T>* minValNode(BinaryTreeNode<T> *root) const;
 	bool find(const T& _value, BinaryTreeNode<T> *root) const;
 
 	void inorderPrintTraversal(BinaryTreeNode<T> *root) const;
@@ -34,7 +36,7 @@ public:
 	~BinarySearchTree<T>() {};
 	
 	void insert(const T& _value);
-	bool erase(const T& _value);
+	void erase(const T& _value);
 	bool find(const T& _value) const;
 	void rebalance();
 
@@ -70,6 +72,48 @@ inline void BinarySearchTree<T>::insert(const T &_value, BinaryTreeNode<T> *root
 }
 
 template<typename T>
+BinaryTreeNode<T>* BinarySearchTree<T>::erase(BinaryTreeNode<T> *root, const T& _value) {
+	if (root == nullptr) return root;
+	if (_value < root->m_value) {
+		//std::cout << "Going left to " << root->left->m_value << std::endl;
+		root->left = erase(root->left, _value);
+	}
+	else if (_value > root->m_value) {
+		//std::cout << "Going right to " << root->right->m_value << std::endl;
+		root->right = erase(root->right, _value);
+	}
+	else {
+		if (root->right == nullptr && root->left == nullptr) {
+			delete root;
+			return nullptr;
+		}
+		else if (root->right == nullptr) {
+			m_cursor = root->left;
+			delete root;
+			return m_cursor;
+		}
+		else if (root->left == nullptr) {
+			m_cursor = root->right;
+			delete root;
+			return m_cursor;
+		}
+		else {
+			m_cursor = minValNode(root->right);
+			root->m_value = m_cursor->m_value;
+			root->right = erase(root->right, m_cursor->m_value);
+		}
+	}
+
+	return root;
+}
+
+template<typename T>
+BinaryTreeNode<T> *BinarySearchTree<T>::minValNode(BinaryTreeNode<T> *root) const {
+	if (root->left != nullptr) return minValNode(root->left);
+	else return root;
+}
+
+template<typename T>
 bool BinarySearchTree<T>::find(const T& _value, BinaryTreeNode<T> *root) const {
 	if (root == nullptr) return false;
 	else if (root->m_value == _value) return true;
@@ -88,8 +132,8 @@ void BinarySearchTree<T>::inorderPrintTraversal(BinaryTreeNode<T> *root) const {
 template<typename T>
 void BinarySearchTree<T>::postorderPrintTraversal(BinaryTreeNode<T> *root) const {
 	if (root == nullptr) return;
-	inorderPrintTraversal(root->left);
-	inorderPrintTraversal(root->right);
+	postorderPrintTraversal(root->left);
+	postorderPrintTraversal(root->right);
 	std::cout << root->m_value << " ";
 }
 
@@ -111,7 +155,6 @@ void BinarySearchTree<T>::insert(const T& _value) {
 	if (m_root == nullptr) m_root = new BinaryTreeNode<T>(_value);
 	else
 		insert(_value, m_root);
-
 }
 
 /*
@@ -120,19 +163,11 @@ algorithm: if left-or-right side of target node is null, have target pointer
 		   if both are full, delete the target and replace it with a leaf from
 		   the left side to rebalance the tree
 */
-/*template<typename T>
-bool BinarySearchTree<T>::erase(const T& _value) {
-	if (m_root->m_value == _value) {
-
-	}
-	BinaryTreeNode<T> *pre_cursor = m_root;
-	if()
-	while (!m_cursor) {
-		if (_value = m_cursor->m_value) {
-
-		}
-	}
-}*/
+template<typename T>
+void BinarySearchTree<T>::erase(const T& _value) {
+	erase(m_root, _value);
+	
+}
 
 template<typename T>
 bool BinarySearchTree<T>::find(const T& _value) const {
